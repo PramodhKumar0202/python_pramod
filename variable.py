d959329402b44915e00690cc603967ab5a11e66f
Pramodh Kumar <pramodhkumar0202@gmail.com>
2024-11-25 11:58:57 +0530

    added details

--- variable.py
+++ variable.py
@@ -1,1 +1,6 @@
-print("welcome to the coding world! ")
+#print("welcome to the coding world! ")
+name ="pramod kumar"
+print(name)
+designation="data engineer"
+location="remote"
+print(f"{name},{designation},{location}")
