2337e83a7a85f16a3c20a7ce9bae446179d54334
Pramodh Kumar <pramodhkumar0202@gmail.com>
2024-11-28 11:10:16 +0530

    added

---
+++ data_types.py
@@ -0,0 +1,25 @@
+# intergers 
+age = 24 
+type(age) # it will gives type of variable 
+
+# float
+height =5.10
+
+
+# string 
+name ="pramod"
+print(type(name))
+
+# boolean 
+is_student=True
+print(type(is_student))
+
+a=6
+b=6
+
+type(a==b)  # it will gives true 
+
+
+adding ="hello" + str(5)
+
+print(adding)
